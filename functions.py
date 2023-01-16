1df23f80d08d60e273cd90cdde01f2ed0d8002d1
t_lan <t_lange90@hotmail.com>
2023-01-16 16:13:44 +0100

    chore: Added time/space complexities refactor: Minor changes to functions
    To show my understanding of Big O

--- functions.py
+++ functions.py
@@ -1,5 +1,5 @@
 def format_duration(secs):
-    """Formats seconds to minutes, days, hours, etc"""
+    """Formats seconds to minutes, days, hours, etc. Time complexity: O(1). Space complexity: O(1)"""
     if secs == 0:
         return "now"
     yy = secs//(365*(24*3600))
@@ -32,20 +32,34 @@ class CaesarCipher:
         self.shift = shift
 
     def encode(self, st):
+        """Time complexity: O(n). Space complexity: O(n)"""
         st = st.upper()
         shift = self.shift
         return "".join([c if not c.isalpha() else chr(ord(c)+shift) if ord(c)+shift in range(65, 91)
                         else chr(ord(c)-26+shift) for c in st])
 
     def decode(self, st):
+        """Time complexity: O(n). Space complexity: O(n)"""
         st = st.upper()
         shift = self.shift
         return "".join([c if not c.isalpha() else chr(ord(c)-shift) if ord(c)-shift in range(65, 91)
                         else chr(ord(c)+26-shift) for c in st])
 
 
+def rgb(r, g, b):
+    """Converts RGB to hex values. Time complexity: O(1). Space complexity: O(1)"""
+    take_closest = lambda num, collection: min(collection, key=lambda x: abs(x - num))
+    rgb_range = [0, 255]
+    r = take_closest(r, rgb_range) if r not in range(255) else r  # O(log n) for lookup in range
+    g = take_closest(g, rgb_range) if g not in range(255) else g
+    b = take_closest(b, rgb_range) if b not in range(255) else b
+    r, g, b = hex(r)[2:], hex(g)[2:], hex(b)[2:]
+    return f"{r.zfill(2)}{g.zfill(2)}{b.zfill(2)}".upper()
+
+
 if __name__ == "__main__":
-    print(format_duration(3754))
     a = CaesarCipher()
-    print(a.encode("Hello world!"))
-    print(a.decode(a.encode("Hello world!")))
+    to_print = [format_duration(3754), a.encode("Hello world!"), a.decode(a.encode("Hello world!")),
+                rgb(145, 233, 999), ]
+    for f in to_print:
+        print(f)
