d53c4c7a2d7d67047b10921bcbd642ed49fdf257
t_lan <t_lange90@hotmail.com>
2023-01-16 15:29:00 +0100

    second formatter func added

--- functions.py
+++ functions.py
@@ -1,3 +1,31 @@
+def format_duration(secs):
+    """Formats seconds to minutes, days, hours, etc"""
+    if secs == 0:
+        return "now"
+    yy = secs//(365*(24*3600))
+    dd = secs//(3600*24) % 365
+    hh = secs//3600 % 24
+    mm = secs//60 % 60
+    ss = secs % 60
+    yyf = "year" if yy == 1 else "years"
+    ddf = "day" if dd == 1 else "days"
+    hhf = "hour" if hh == 1 else "hours"
+    mmf = "minute" if mm == 1 else "minutes"
+    ssf = "second" if ss == 1 else "seconds"
+    construct = [f"{x[0]} {x[1]}" for x in list(zip([yy, dd, hh, mm, ss], [yyf, ddf, hhf, mmf, ssf])) if x[0] != 0]
+    if len(construct) == 1:
+        return construct[0]
+    correct = ""
+    for i, e in enumerate(construct, 1):
+        if i == 1:
+            correct += e
+        elif i != len(construct):
+            correct += ", "+e
+        else:
+            correct += " and "+e
+    return correct
+
+
 class CaesarCipher:
     """Encodes to and from a caesar cipher"""
     def __init__(self, shift):
