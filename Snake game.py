c0fd78f20041b6cb7f1d6a0c6952298ed4e0c49b
Langeball <105056943+Langeball@users.noreply.github.com>
2023-05-12 16:17:24 +0200

    Update Snake game.py
    func: Added score system and paused screen at start
    refactor: Moved some food logic from collision() to place_food()

--- Snake game.py
+++ Snake game.py
@@ -1,37 +1,52 @@
 import pygame
 from collections import deque
-import random
-# import asyncio
+from random import randrange
+from sys import exit
 
 def main():
-    """Game loop"""
-    global direction
-    global head
+    global direction, head, food_location
 
+    # Prevent game starting immediately
+    pygame.display.set_caption("Press any key to start")
+    pygame.display.update()
+    pause = True
+    while pause:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over(False)
+            # Start game if any button is pressed
+            elif event.type == pygame.KEYDOWN:
+                pause = False
+    # Game loop
     while True:
         # Pygame logic
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                raise StopIteration
+                game_over(False)
 
             # Check for key press
             if event.type == pygame.KEYDOWN:
-                # Check which key was pressed
-                if event.key == pygame.K_LEFT:
+                # Check which key was pressed and prevent moving opposite direction
+                if event.key == pygame.K_LEFT and direction != "right":
                     direction = "left"
-                elif event.key == pygame.K_RIGHT:
+                elif event.key == pygame.K_RIGHT and direction != "left":
                     direction = "right"
-                elif event.key == pygame.K_DOWN:
+                elif event.key == pygame.K_DOWN and direction != "up":
                     direction = "down"
-                elif event.key == pygame.K_UP:
+                elif event.key == pygame.K_UP and direction != "down":
                     direction = "up"
 
         # Game logic
-        head = control()
-        if collision():
-            delete_tail(*tail)
+        head = move_head()
+        collision()
         draw_cube(*head)
+        if not food_location:
+            food_location = place_food()
+        else:
+            delete_tail()
+
+        # Update score
+        pygame.display.set_caption(f"Current score: {score}")
 
         # Update screen and frame-rate
         pygame.display.update()
@@ -43,48 +58,48 @@ def draw_cube(x, y):
     snake[(x, y)] = (x, y)
 
 
-def delete_tail(x, y):
+def delete_tail():
     """Oldest part of snake body becomes new tail, old tail erased."""
     global tail
-    tail = body.popleft()
+    x, y = tail
     pygame.draw.rect(screen, background_colour, (x, y, cube_size, cube_size))
+    tail = body.popleft()
 
     # For collision detection
     if (x, y) in snake:
         del snake[(x, y)]
 
 def collision():
-    """Detects border, food and snake body collision. TODO: Move food collision to its own func"""
-    global food_location
-    # If head eats food, returning False prevents tail deletion
+    """Detects border, food and snake body collision."""
+    global food_location, score
+    # If head eats food
     if head == food_location:
-        food_location = place_food()
-        return False
+        food_location = None
+        score += 1
 
     # Border control
     x, y = head
     if (x < 0 or x >= board_size) or (y < 0 or y >= board_size):
-        pygame.quit()
-        raise StopIteration
+        game_over()
 
     # Snake collision
     if head in snake:
-        pygame.quit()
-        raise StopIteration
+        game_over()
 
     return True
 
 def place_food():
-    """Randrange will eventually slow down game horrifically. TODO: asyncio it"""
-    while True:
-        x, y = random.randrange(0, board_size, cube_size), random.randrange(0, board_size, cube_size)
+    """Using a count we prevent the possibility of bad RNG on food placement"""
+    count = 0
+    while count < 100:  # To prevent possible lag, we allow the possibility of no food for a while
+        x, y = randrange(0, board_size, cube_size), randrange(0, board_size, cube_size)
         # Prevent food from being placed inside snake
         if (x, y) not in snake:
-            break
-    pygame.draw.rect(screen, food_colour, (x, y, cube_size, cube_size))
-    return x, y
+            pygame.draw.rect(screen, food_colour, (x, y, cube_size, cube_size))
+            return x, y
+        count += 1
 
-def control():
+def move_head():
     """Depending on direction, add/sub to either x or y. Change direction"""
     # Old head becomes part of body
     body.append(head)
@@ -100,6 +115,24 @@ def control():
         y -= cube_size
     return x, y
 
+def game_over(show_score=True):
+    if show_score:
+        font = pygame.font.Font(None, 36)
+        text = f"Final score: {score}"
+        text_surface = font.render(text, True, (255, 255, 255))
+        text_x = board_size // 2 - text_surface.get_width() // 2
+        text_y = 50
+        screen.blit(text_surface, (text_x, text_y))
+        pygame.display.set_caption("Close window and try again!")
+        pygame.display.update()
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    game_over(False)
+
+    pygame.quit()
+    exit()
+
 # Setting up pygame board/screen
 pygame.init()
 board_size = 400  # x and y value
@@ -113,7 +146,7 @@ cube_size = 25  # x and y value
 cube_colour = "red"
 head = (board_size // 2, board_size // 2)  # Middle of board
 tail = ((board_size // 2) - (cube_size * 2), board_size // 2)  # 2 cubes behind head
-body = deque([((board_size // 2) - cube_size, board_size // 2)])  # 1 cube behind head
+body = deque([((board_size // 2) - cube_size, board_size // 2), ])  # 1 cube behind head
 snake = {}
 direction = "right"
 # Draw the starting 3 length snake
@@ -124,6 +157,9 @@ for e in (tail, body[0], head):
 food_colour = "green"
 food_location = place_food()
 
+# Score
+score = 0
+
 if __name__ == "__main__":
     main()
-
+    
